492c31269c1fc3a088305d5ca2f4e20a072d8089
Tim Spence <timothywspence@gmail.com>
2017-11-06 15:32:37 +0000

    EID-273: rewrite validator to integrate with new validation framework
    Authors: @timwspence

---
+++ src/main/java/uk/gov/ida/matchingserviceadapter/validators/SubjectConfirmationDataValidator.java
@@ -0,0 +1,51 @@
+package uk.gov.ida.matchingserviceadapter.validators;
+
+import org.beanplanet.messages.domain.MessageImpl;
+import org.beanplanet.validation.CompositeValidator;
+import org.beanplanet.validation.FixedErrorValidator;
+import org.beanplanet.validation.RequiredValidator;
+import org.opensaml.saml.saml2.core.SubjectConfirmationData;
+
+import java.util.function.Function;
+
+public class SubjectConfirmationDataValidator<T> extends CompositeValidator<T> {
+
+    public static final MessageImpl CONFIRMATION_DATA_NOT_PRESENT = MessageImpl.globalMessage("confirmationData.absent", "confirmation data must not be null");
+    public static final MessageImpl NOT_ON_OR_AFTER_NOT_PRESENT = MessageImpl.fieldMessage("confirmationData.onOrAfter", "confirmationData.onOrAfter.absent", "Not on or after must not be null");
+    public static final MessageImpl NOT_ON_OR_AFTER_INVALID = MessageImpl.fieldMessage("confirmationData.notOnOrAfter", "confirmationData.notOnOrAfter.invalid", "Not on or after must not be in the past");
+    public static final MessageImpl NOT_BEFORE_INVALID = MessageImpl.fieldMessage("confirmationData.notBefore", "confirmationData.notBefore.invalid", "Not before must not be in the future");
+    public static final MessageImpl IN_RESPONSE_TO_NOT_PRESENT = MessageImpl.fieldMessage("confirmationData.inResponseTo", "confirmationData.inResponseTo.absent", "In response to must not be null");
+    public static final MessageImpl IN_RESPONSE_TO_NOT_WHAT_WAS_EXPECTED = MessageImpl.fieldMessage("confirmationData.inResponseTo", "confirmationData.inResponseTo.incorrect", "In response to was not what was expected");
+    public static final MessageImpl RECIPIENT_NOT_PRESENT = MessageImpl.fieldMessage("confirmationData.recipient", "confirmationData.recipient.absent", "Recipient must not be null");
+
+    public SubjectConfirmationDataValidator(Function<T, SubjectConfirmationData> valueProvider, TimeRestrictionValidator timeRestrictionValidator, String requestId) {
+        super(
+            true,
+            valueProvider,
+            new RequiredValidator<>(CONFIRMATION_DATA_NOT_PRESENT),
+            new RequiredValidator<>(NOT_ON_OR_AFTER_NOT_PRESENT, SubjectConfirmationData::getNotOnOrAfter),
+            new FixedErrorValidator<>(scd -> {
+                try {
+                    timeRestrictionValidator.validateNotOnOrAfter(scd.getNotOnOrAfter());
+                    return false;
+                }
+                catch(Exception e) {
+                    return true;
+                }
+            }, NOT_ON_OR_AFTER_INVALID),
+            new FixedErrorValidator<>(scd -> {
+                try {
+                    timeRestrictionValidator.validateNotBefore(scd.getNotBefore());
+                    return false;
+                }
+                catch(Exception e) {
+                    return true;
+                }
+            }, NOT_BEFORE_INVALID),
+            new RequiredValidator<>(IN_RESPONSE_TO_NOT_PRESENT, SubjectConfirmationData::getInResponseTo),
+            new FixedErrorValidator<>(scd -> !scd.getInResponseTo().equals(requestId), IN_RESPONSE_TO_NOT_WHAT_WAS_EXPECTED),
+            new RequiredValidator<>(RECIPIENT_NOT_PRESENT, SubjectConfirmationData::getRecipient)
+        );
+    }
+
+}
--- src/main/java/uk/gov/ida/matchingserviceadapter/validators/SubjectValidator.java
+++ src/main/java/uk/gov/ida/matchingserviceadapter/validators/SubjectValidator.java
@@ -1,73 +1,38 @@
 package uk.gov.ida.matchingserviceadapter.validators;
 
 import com.google.common.collect.ImmutableSet;
-import org.joda.time.DateTime;
+import org.beanplanet.messages.domain.MessageImpl;
+import org.beanplanet.validation.CompositeValidator;
+import org.beanplanet.validation.FixedErrorValidator;
+import org.beanplanet.validation.RequiredValidator;
 import org.opensaml.saml.saml2.core.NameID;
 import org.opensaml.saml.saml2.core.Subject;
-import org.opensaml.saml.saml2.core.SubjectConfirmation;
-import org.opensaml.saml.saml2.core.SubjectConfirmationData;
-import uk.gov.ida.matchingserviceadapter.validators.exceptions.SamlResponseValidationException;
 
 import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.function.Function;
 
-import static org.opensaml.saml.saml2.core.SubjectConfirmation.METHOD_BEARER;
+import static org.beanplanet.messages.domain.MessageImpl.fieldMessage;
+import static org.beanplanet.messages.domain.MessageImpl.globalMessage;
 
-public class SubjectValidator {
-    private final TimeRestrictionValidator timeRestrictionValidator;
+public class SubjectValidator<T> extends CompositeValidator<T> {
+
+    public static final MessageImpl SUBJECT_NOT_PRESENT = globalMessage("subject", "Subject not present");
+    public static final MessageImpl WRONG_NUMBER_OF_SUBJECT_CONFIRMATIONS = fieldMessage("subjectConfirmations", "subject.subjectConfirmations.wrong.size", "Must have exactly 1 subject confirmation");
+    public static final MessageImpl NAME_ID_NOT_PRESENT = fieldMessage("subject.nameId", "subject.nameId.absent", "NameID not present");
+    public static final MessageImpl NAME_ID_IN_WRONG_FORMAT = fieldMessage("subject.nameId", "subject.nameId.wrong.format", "NameID not in valid format");
+
     public static final Set<String> VALID_IDENTIFIERS = ImmutableSet.of(NameID.PERSISTENT);
 
-    public SubjectValidator(TimeRestrictionValidator timeRestrictionValidator) {
-        this.timeRestrictionValidator = timeRestrictionValidator;
+    public SubjectValidator(Function<T, Subject> valueProvider, TimeRestrictionValidator timeRestrictionValidator, String requestId) {
+        super(
+            true,
+            valueProvider,
+            new RequiredValidator<>(SUBJECT_NOT_PRESENT),
+            new FixedErrorValidator<>(subject -> subject.getSubjectConfirmations().size() != 1, WRONG_NUMBER_OF_SUBJECT_CONFIRMATIONS),
+            new RequiredValidator<>(NAME_ID_NOT_PRESENT, Subject::getNameID),
+            new FixedErrorValidator<>(subject -> !VALID_IDENTIFIERS.contains(subject.getNameID().getFormat()), NAME_ID_IN_WRONG_FORMAT),
+            new SubjectConfirmationValidator<>(subject -> subject.getSubjectConfirmations().get(0), timeRestrictionValidator, requestId)
+        );
     }
 
-    public void validate(Subject subject, String expectedInResponseTo) {
-        if (subject == null) {
-            throw new SamlResponseValidationException("Subject is missing from the assertion.");
-        }
-
-        if (subject.getSubjectConfirmations().size() != 1) {
-            throw new SamlResponseValidationException("Exactly one subject confirmation is expected.");
-        }
-
-        SubjectConfirmation subjectConfirmation = subject.getSubjectConfirmations().get(0);
-        if (!METHOD_BEARER.equals(subjectConfirmation.getMethod())) {
-            throw new SamlResponseValidationException("Subject confirmation method must be 'bearer'.");
-        }
-
-        SubjectConfirmationData subjectConfirmationData = subjectConfirmation.getSubjectConfirmationData();
-        if (subjectConfirmationData == null) {
-            throw new SamlResponseValidationException("Subject confirmation data is missing from the assertion.");
-        }
-
-        timeRestrictionValidator.validateNotBefore(subjectConfirmationData.getNotBefore());
-
-        DateTime notOnOrAfter = subjectConfirmationData.getNotOnOrAfter();
-        if (notOnOrAfter == null) {
-            throw new SamlResponseValidationException("Subject confirmation data must contain 'NotOnOrAfter'.");
-        }
-
-        timeRestrictionValidator.validateNotOnOrAfter(notOnOrAfter);
-
-        String actualInResponseTo = subjectConfirmationData.getInResponseTo();
-        if (actualInResponseTo == null) {
-            throw new SamlResponseValidationException("Subject confirmation data must contain 'InResponseTo'.");
-        }
-
-        if (!expectedInResponseTo.equals(actualInResponseTo)) {
-            throw new SamlResponseValidationException(String.format("'InResponseTo' must match requestId. Expected %s but was %s", expectedInResponseTo, actualInResponseTo));
-        }
-
-        if (subject.getNameID() == null) {
-            throw new SamlResponseValidationException("NameID is missing from the subject of the assertion.");
-        }
-
-        if (!VALID_IDENTIFIERS.contains(subject.getNameID().getFormat())) {
-            throw new SamlResponseValidationException(String.format("NameID [%s] is not in the correct format. It needs to be %s", subject.getNameID().getFormat(), VALID_IDENTIFIERS.stream().collect(Collectors.joining(","))));
-        }
-
-        if (null == subjectConfirmationData.getRecipient()) {
-            throw new SamlResponseValidationException("Subject confirmation data must contain 'Recipient'.");
-        }
-    }
 }
