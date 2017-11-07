b1e636d797c0f355a3e4c50492abd006db92704b
Hristo Georgiev <hristo.georgiev@digital.cabinet-office.gov.uk>
2017-11-07 11:00:02 +0000

    TT-1514: Ignores unknown configuration properties.
    Added annotations to ignore unknown properties within the configuration file.
    
    Authors: @richardtowers @georgievh

--- src/main/java/uk/gov/ida/verifymatchingservicetesttool/configurations/LocalMatchingServiceConfiguration.java
+++ src/main/java/uk/gov/ida/verifymatchingservicetesttool/configurations/LocalMatchingServiceConfiguration.java
@@ -1,11 +1,13 @@
 package uk.gov.ida.verifymatchingservicetesttool.configurations;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.net.URI;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LocalMatchingServiceConfiguration {
 
     @NotNull
