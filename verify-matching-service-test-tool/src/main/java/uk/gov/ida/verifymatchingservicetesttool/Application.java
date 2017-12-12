16529ab992d7e2488c900110eee6e2b6221f9971
Simon Greensmith <simon.greensmith@digital.cabinet-office.gov.uk>
2017-12-12 12:08:53 +0000

    TT-1621 add exit code for failing scenarios
    Authors: @sgreensmith @andy-paine

--- verify-matching-service-test-tool/src/main/java/uk/gov/ida/verifymatchingservicetesttool/Application.java
+++ verify-matching-service-test-tool/src/main/java/uk/gov/ida/verifymatchingservicetesttool/Application.java
@@ -11,6 +11,7 @@ import uk.gov.ida.verifymatchingservicetesttool.configurations.ConfigurationRead
 import uk.gov.ida.verifymatchingservicetesttool.resolvers.ApplicationConfigurationResolver;
 import uk.gov.ida.verifymatchingservicetesttool.resolvers.FilesLocatorResolver;
 import uk.gov.ida.verifymatchingservicetesttool.utils.DefaultFilesLocator;
+import uk.gov.ida.verifymatchingservicetesttool.utils.ExitStatus;
 import uk.gov.ida.verifymatchingservicetesttool.utils.FilesLocator;
 import uk.gov.ida.verifymatchingservicetesttool.utils.TestStatusPrintingListener;
 
@@ -19,15 +20,16 @@ import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPacka
 public class Application {
 
     public static void main(String[] args) {
-        new Application().execute(
+        ExitStatus exitStatus = new Application().execute(
             new TestStatusPrintingListener(),
             selectPackage("uk.gov.ida.verifymatchingservicetesttool.scenarios"),
             ConfigurationReader.getConfiguration(),
             new DefaultFilesLocator()
         );
+        System.exit(exitStatus.getExitCode());
     }
 
-    public void execute(
+    public ExitStatus execute(
         SummaryGeneratingListener listener,
         DiscoverySelector selector,
         ApplicationConfiguration applicationConfiguration,
@@ -43,5 +45,7 @@ public class Application {
         Launcher launcher = LauncherFactory.create();
         launcher.registerTestExecutionListeners(listener);
         launcher.execute(request);
+
+        return listener.getSummary().getFailures().isEmpty() ? ExitStatus.SUCCESS : ExitStatus.FAILURE;
     }
 }
--- verify-matching-service-test-tool/src/test/java/feature/uk/gov/ida/verifymatchingservicetesttool/ApplicationScenariosExecutionFeatureTest.java
+++ verify-matching-service-test-tool/src/test/java/feature/uk/gov/ida/verifymatchingservicetesttool/ApplicationScenariosExecutionFeatureTest.java
@@ -3,9 +3,14 @@ package feature.uk.gov.ida.verifymatchingservicetesttool;
 import org.junit.jupiter.api.Test;
 import uk.gov.ida.verifymatchingservicetesttool.configurations.ApplicationConfiguration;
 import uk.gov.ida.verifymatchingservicetesttool.utils.DefaultFilesLocator;
+import uk.gov.ida.verifymatchingservicetesttool.utils.ExitStatus;
 import uk.gov.ida.verifymatchingservicetesttool.utils.TestStatusPrintingListener;
 
 import static common.uk.gov.ida.verifymatchingservicetesttool.builders.ApplicationConfigurationBuilder.aApplicationConfiguration;
+import static common.uk.gov.ida.verifymatchingservicetesttool.services.LocalMatchingServiceStub.MatchingResult.MATCH;
+import static common.uk.gov.ida.verifymatchingservicetesttool.services.LocalMatchingServiceStub.MatchingResult.NO_MATCH;
+import static common.uk.gov.ida.verifymatchingservicetesttool.services.LocalMatchingServiceStub.RELATIVE_MATCH_URL;
+import static javax.ws.rs.core.Response.Status.OK;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
@@ -30,9 +35,52 @@ public class ApplicationScenariosExecutionFeatureTest extends FeatureTestBase {
         );
 
         assertThat(
-            "There are failures. Check the printed result above.",
-            listener.getSummary().getFailures().isEmpty(),
-            is(true)
+            listener.getSummary().getTestsStartedCount(),
+            is(8L)
         );
+    }
+
+    @Test
+    public void shouldReturnFailureExitCodeIfAnyScenariosFail() {
+
+        localMatchingService.ensureResponseFor(
+            RELATIVE_MATCH_URL,
+            OK,
+            String.format("{\"result\": \"%s\"}", NO_MATCH.getResult())
+        );
+
+        ApplicationConfiguration applicationConfiguration = aApplicationConfiguration()
+            .withLocalMatchingServiceMatchUrl(localMatchingService.getMatchingUrl())
+            .withLocalMatchingServiceAccountCreationUrl(localMatchingService.getAccountCreationUrl())
+            .build();
+
+        ExitStatus exitStatus = application.execute(
+            listener,
+            selectPackage("uk.gov.ida.verifymatchingservicetesttool.scenarios"),
+            applicationConfiguration,
+            fileLocator
+        );
+
+        assertThat(exitStatus, is(ExitStatus.FAILURE));
+    }
+
+    @Test
+    public void shouldReturnSuccessExitCodeIfAllScenariosPass() {
+
+        localMatchingService.ensureDefaultMatchScenariosExist();
+
+        ApplicationConfiguration applicationConfiguration = aApplicationConfiguration()
+            .withLocalMatchingServiceMatchUrl(localMatchingService.getMatchingUrl())
+            .withLocalMatchingServiceAccountCreationUrl(localMatchingService.getAccountCreationUrl())
+            .build();
+
+        ExitStatus exitStatus = application.execute(
+            listener,
+            selectPackage("uk.gov.ida.verifymatchingservicetesttool.scenarios"),
+            applicationConfiguration,
+            fileLocator
+        );
+
+        assertThat(exitStatus, is(ExitStatus.SUCCESS));
     }
 }
