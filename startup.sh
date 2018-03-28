4b301715f548c631322e38b15cdd435ff526b16b
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-03-28 10:16:03 +0100

    bau: Fix local startup to source env vars properly
    Co-authored-by: Hoque Ali <ali.hoque@digital.cabinet-office.gov.uk>

--- startup.sh
+++ startup.sh
@@ -3,7 +3,9 @@
 
 # Use the local environment if one exists
 if test -e local.env; then
+    set -a
     source local.env
+    set +a
     port=$TEST_RP_MSA_PORT
 else
     printf "$(tput setaf 1)No local environment found. Use verify-local-startup or openssl to generate a local.env file\n$(tput sgr0)"
