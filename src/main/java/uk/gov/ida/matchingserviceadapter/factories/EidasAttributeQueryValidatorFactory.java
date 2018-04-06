b2b729c87c0930c5279dbc4417733aecba35d5a2
Irene Lau <irene.lau@digital.cabinet-office.gov.uk>
2018-04-06 15:16:11 +0100

    BAU: Use later version of hub-saml and verify-saml-libs with fixes
    Solo: @IreneLau-GDS

--- src/main/java/uk/gov/ida/matchingserviceadapter/factories/EidasAttributeQueryValidatorFactory.java
+++ src/main/java/uk/gov/ida/matchingserviceadapter/factories/EidasAttributeQueryValidatorFactory.java
@@ -6,30 +6,27 @@ import org.opensaml.saml.saml2.core.AttributeQuery;
 import uk.gov.ida.common.shared.security.X509CertificateFactory;
 import uk.gov.ida.matchingserviceadapter.MatchingServiceAdapterConfiguration;
 import uk.gov.ida.matchingserviceadapter.repositories.CertificateExtractor;
-import uk.gov.ida.matchingserviceadapter.repositories.CertificateValidator;
 import uk.gov.ida.matchingserviceadapter.validators.DateTimeComparator;
 import uk.gov.ida.matchingserviceadapter.validators.EidasAttributeQueryValidator;
+import uk.gov.ida.matchingserviceadapter.validators.exceptions.SamlResponseValidationException;
 import uk.gov.ida.saml.metadata.EidasMetadataResolverRepository;
 import uk.gov.ida.saml.security.AssertionDecrypter;
 import uk.gov.ida.validation.validators.Validator;
 
 public class EidasAttributeQueryValidatorFactory {
     private final MetadataResolver verifyMetadataResolver;
-    private final CertificateValidator verifyCertificateValidator;
     private final X509CertificateFactory x509CertificateFactory;
     private final MatchingServiceAdapterConfiguration configuration;
     private final AssertionDecrypter assertionDecrypter;
     private final EidasMetadataResolverRepository eidasMetadataResolverRepository;
 
     public EidasAttributeQueryValidatorFactory(MetadataResolver verifyMetadataResolver,
-                                               CertificateValidator verifyCertificateValidator,
                                                X509CertificateFactory x509CertificateFactory,
                                                MatchingServiceAdapterConfiguration configuration,
                                                AssertionDecrypter assertionDecrypter,
                                                EidasMetadataResolverRepository eidasMetadataResolverRepository) {
 
         this.verifyMetadataResolver = verifyMetadataResolver;
-        this.verifyCertificateValidator = verifyCertificateValidator;
         this.x509CertificateFactory = x509CertificateFactory;
         this.configuration = configuration;
         this.assertionDecrypter = assertionDecrypter;
@@ -39,7 +36,8 @@ public class EidasAttributeQueryValidatorFactory {
     public Validator<AttributeQuery> build(String issuerEntityId) {
         return new EidasAttributeQueryValidator(
                 verifyMetadataResolver,
-                eidasMetadataResolverRepository.getMetadataResolver(issuerEntityId),
+                eidasMetadataResolverRepository.getMetadataResolver(issuerEntityId).orElseThrow(() ->
+                        new SamlResponseValidationException("Unable to find metadata resolver for entity Id " + issuerEntityId)),
                 new CertificateExtractor(),
                 x509CertificateFactory,
                 new DateTimeComparator(Duration.standardSeconds(configuration.getClockSkew())),
