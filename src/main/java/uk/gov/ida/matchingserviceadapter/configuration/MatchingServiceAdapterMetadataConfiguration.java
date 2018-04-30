766bee1fed0b9468cf4ed1d5fd6795c1de3b8581
Aditya Pahuja <aditya.pahuja@digital.cabinet-office.gov.uk>
2018-04-30 23:03:23 +0100

    HUB-102: Update to use two new truststores (hub and idp)
    Update to use latest version of verify-saml-libs and verify-hub-saml.
    Update matching service adaptor to use hub truststore and identity
    provider truststore to enable metadata resolver to perform certificate
    chain validation on hub SAML signing and encryption certificates and
    identity providers SAML signing certificates in hub federation metadata
    respectively.
    Add four new truststores (hub and identity provider) for integration
    environment and production environment. Clients do not need to
    configure matching service adaptor config files to use hub truststore
    and identity provider truststore.
    
    Co-authored-by: Rachel Smith <rachel.smith@digital.cabinet-office.gov.uk>

---
+++ src/main/java/uk/gov/ida/matchingserviceadapter/configuration/MatchingServiceAdapterMetadataConfiguration.java
@@ -0,0 +1,72 @@
+package uk.gov.ida.matchingserviceadapter.configuration;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.client.JerseyClientConfiguration;
+import uk.gov.ida.saml.metadata.TrustStoreBackedMetadataConfiguration;
+import uk.gov.ida.saml.metadata.TrustStoreConfiguration;
+import uk.gov.ida.saml.metadata.exception.EmptyTrustStoreException;
+
+import java.net.URI;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.util.Optional;
+
+public class MatchingServiceAdapterMetadataConfiguration extends TrustStoreBackedMetadataConfiguration {
+
+    private final MatchingServiceAdapterEnvironment environment;
+
+    private final TrustStoreConfiguration hubTrustStore;
+
+    private final TrustStoreConfiguration idpTrustStore;
+
+    @JsonCreator
+    public MatchingServiceAdapterMetadataConfiguration(
+        @JsonProperty("uri") @JsonAlias({ "url" }) URI uri,
+        @JsonProperty("minRefreshDelay") Long minRefreshDelay,
+        @JsonProperty("maxRefreshDelay") Long maxRefreshDelay,
+        @JsonProperty("expectedEntityId") String expectedEntityId,
+        @JsonProperty("client") JerseyClientConfiguration client,
+        @JsonProperty("jerseyClientName") String jerseyClientName,
+        @JsonProperty("hubFederationId") String hubFederationId,
+        @JsonProperty("trustStore") TrustStoreConfiguration trustStore,
+        @JsonProperty("hubTrustStore") TrustStoreConfiguration hubTrustStore,
+        @JsonProperty("idpTrustStore") TrustStoreConfiguration idpTrustStore,
+        @JsonProperty("environment") MatchingServiceAdapterEnvironment environment) {
+
+        super(uri, minRefreshDelay, maxRefreshDelay, expectedEntityId, client, jerseyClientName, hubFederationId, trustStore);
+        this.hubTrustStore = hubTrustStore;
+        this.idpTrustStore = idpTrustStore;
+        this.environment = Optional.ofNullable(environment).orElse(MatchingServiceAdapterEnvironment.INTEGRATION);
+    }
+
+    @Override
+    public Optional<KeyStore> getHubTrustStore() {
+        return Optional.of(
+            validateTruststore(
+                Optional.ofNullable(hubTrustStore)
+                        .orElseGet(() -> new DefaultHubTrustStoreConfiguration(environment)).getTrustStore()));
+    }
+
+    @Override
+    public Optional<KeyStore> getIdpTrustStore() {
+        return Optional.of(
+            validateTruststore(
+                Optional.ofNullable(idpTrustStore)
+                        .orElseGet(() -> new DefaultIdentityProviderTrustStoreConfiguration(environment)).getTrustStore()));
+    }
+
+    private KeyStore validateTruststore(KeyStore trustStore) {
+        int trustStoreSize;
+        try {
+            trustStoreSize = trustStore.size();
+        } catch (KeyStoreException e) {
+            throw new RuntimeException(e);
+        }
+        if (trustStoreSize == 0) {
+            throw new EmptyTrustStoreException();
+        }
+        return trustStore;
+    }
+}
