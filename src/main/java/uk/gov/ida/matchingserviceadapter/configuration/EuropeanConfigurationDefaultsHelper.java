ebd48f5fdb624db00f550024c4156046f5096968
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2020-09-10 17:30:42 +0100

    HUB-697: Update connector-node metadata URL
    We now have a qwac cert for the connector node, and so have a firmer
    idea of what the URL for the metadata will be.

--- src/main/java/uk/gov/ida/matchingserviceadapter/configuration/EuropeanConfigurationDefaultsHelper.java
+++ src/main/java/uk/gov/ida/matchingserviceadapter/configuration/EuropeanConfigurationDefaultsHelper.java
@@ -1,28 +1,19 @@
 package uk.gov.ida.matchingserviceadapter.configuration;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.eclipse.jetty.util.TypeUtil.asList;
 
 public class EuropeanConfigurationDefaultsHelper {
 
     private static final String[] ACCEPTABLE_HUB_CONNECTOR_ENTITY_IDS_PRODUCTION = new String[] {
-        "https://www.signin.service.gov.uk/SAML2/metadata/connector",               // (AWS)
-        "https://connector-node.london.verify.govsvc.uk/ConnectorMetadata",         // (GSP)
-        "https://eidas.signin.service.gov.uk/ConnectorMetadata",                    // (potential new #1)
-        "https://connector.eidas.signin.service.gov.uk/ConnectorMetadata",          // (potential new #2)
-        "https://connector-node.eidas.signin.service.gov.uk/ConnectorMetadata",     // (potential new #3)
+        "https://www.signin.service.gov.uk/SAML2/metadata/connector",
+        "https://connector-node.eidas.signin.service.gov.uk/ConnectorMetadata",
     };
 
     private static final String[] ACCEPTABLE_HUB_CONNECTOR_ENTITY_IDS_INTEGRATION = new String[]{
-        "https://www.integration.signin.service.gov.uk/SAML2/metadata/connector",               // (AWS)
-        "https://connector-node-integration.london.verify.govsvc.uk/ConnectorMetadata",         // (GSP)
-        "https://eidas.integration.signin.service.gov.uk/ConnectorMetadata",                    // (potential new #1)
-        "https://connector.eidas.integration.signin.service.gov.uk/ConnectorMetadata",          // (potential new #2)
-        "https://connector-node.eidas.integration.signin.service.gov.uk/ConnectorMetadata",     // (potential new #3)
+        "https://www.integration.signin.service.gov.uk/SAML2/metadata/connector",
+        "https://connector-node.integration.eidas.signin.service.gov.uk/ConnectorMetadata",
     };
 
     public static List<String> getDefaultAcceptableHubConnectorEntityIds(MatchingServiceAdapterEnvironment environment) {
