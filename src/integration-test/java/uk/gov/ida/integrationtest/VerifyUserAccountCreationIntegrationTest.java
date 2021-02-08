89dd6d600e67e807525d87d39c1a63e9470aa183
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2021-02-08 16:50:04 +0000

    Remove eIDAS code and config except for europeanIdentity.enabled config flag

--- src/integration-test/java/uk/gov/ida/integrationtest/VerifyUserAccountCreationIntegrationTest.java
+++ src/integration-test/java/uk/gov/ida/integrationtest/VerifyUserAccountCreationIntegrationTest.java
@@ -3,38 +3,16 @@ package uk.gov.ida.integrationtest;
 import httpstub.HttpStubRule;
 import io.dropwizard.testing.ConfigOverride;
 import org.junit.ClassRule;
-import org.junit.Test;
-import org.opensaml.saml.saml2.core.Assertion;
-import org.opensaml.saml.saml2.core.Attribute;
-import org.opensaml.saml.saml2.core.AttributeQuery;
-import org.opensaml.saml.saml2.core.Response;
-import uk.gov.ida.integrationtest.helpers.AttributeFactory;
 import uk.gov.ida.integrationtest.helpers.MatchingServiceAdapterAppRule;
-import uk.gov.ida.saml.core.OpenSamlXmlObjectFactory;
-
-import java.util.List;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.opensaml.saml.saml2.core.StatusCode.REQUESTER;
-import static uk.gov.ida.integrationtest.helpers.AssertionHelper.aValidEidasAttributeQueryWithCycle3Attributes;
-import static uk.gov.ida.integrationtest.helpers.RequestHelper.makeAttributeQueryRequest;
-import static uk.gov.ida.matchingserviceadapter.domain.UserAccountCreationAttribute.*;
-import static uk.gov.ida.saml.core.test.TestCertificateStrings.TEST_RP_MS_PRIVATE_SIGNING_KEY;
-import static uk.gov.ida.saml.core.test.TestCertificateStrings.TEST_RP_MS_PUBLIC_SIGNING_CERT;
-import static uk.gov.ida.saml.core.test.TestEntityIds.HUB_ENTITY_ID;
-import static uk.gov.ida.saml.core.test.TestEntityIds.TEST_RP_MS;
-import static uk.gov.ida.saml.core.test.matchers.SignableSAMLObjectBaseMatcher.signedBy;
 
 public class VerifyUserAccountCreationIntegrationTest extends UserAccountCreationBaseTest {
-    private static final boolean IS_COUNTRY_ENABLED = false;
+    private static final boolean COUNTRY_ENABLED_FALSE = false;
 
     @ClassRule
     public static final HttpStubRule localMatchingService = new HttpStubRule();
     @ClassRule
     public static final MatchingServiceAdapterAppRule applicationRule = new MatchingServiceAdapterAppRule(
-        IS_COUNTRY_ENABLED,
+        COUNTRY_ENABLED_FALSE,
         ConfigOverride.config("localMatchingService.matchUrl", "http://localhost:" + localMatchingService.getPort() + MATCHING_REQUEST_PATH),
         ConfigOverride.config("localMatchingService.accountCreationUrl", "http://localhost:" + localMatchingService.getPort() + UNKNOWN_USER_MATCHING_PATH)
     );
@@ -50,21 +28,4 @@ public class VerifyUserAccountCreationIntegrationTest extends UserAccountCreatio
         return localMatchingService;
     }
 
-    @Test
-    public void shouldReturnFailureResponseWhenPassedEidasFullMatchingDataset() {
-        List<Attribute> requiredAttributes = Stream.of(FIRST_NAME, FIRST_NAME_VERIFIED, MIDDLE_NAME, MIDDLE_NAME_VERIFIED, SURNAME, SURNAME_VERIFIED, CURRENT_ADDRESS, CURRENT_ADDRESS_VERIFIED, ADDRESS_HISTORY, CYCLE_3)
-                .map(userAccountCreationAttribute -> new AttributeFactory(new OpenSamlXmlObjectFactory()).createAttribute(userAccountCreationAttribute))
-                .collect(toList());
-
-        AttributeQuery attributeQuery = aValidEidasAttributeQueryWithCycle3Attributes(REQUEST_ID, applicationRule.getCountryEntityId()).withAttributes(requiredAttributes).build();
-
-        Response response = makeAttributeQueryRequest(UNKNOWN_USER_URI, attributeQuery, signatureAlgorithmForHub, digestAlgorithmForHub, HUB_ENTITY_ID);
-        List<Assertion> decryptedAssertions = assertionDecrypter.decryptAssertions(response::getEncryptedAssertions);
-
-        assertThat(response.getStatus().getStatusCode().getValue()).isEqualTo(REQUESTER);
-        assertThat(decryptedAssertions).hasSize(0);
-        assertThat(response.getInResponseTo()).isEqualTo(REQUEST_ID);
-        assertThat(response.getIssuer().getValue()).isEqualTo(TEST_RP_MS);
-        assertThat(response).is(signedBy(TEST_RP_MS_PUBLIC_SIGNING_CERT, TEST_RP_MS_PRIVATE_SIGNING_KEY));
-    }
 }
--- src/main/java/uk/gov/ida/matchingserviceadapter/configuration/EuropeanIdentityConfiguration.java
+++ src/main/java/uk/gov/ida/matchingserviceadapter/configuration/EuropeanIdentityConfiguration.java
@@ -1,47 +1,16 @@
 package uk.gov.ida.matchingserviceadapter.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import uk.gov.ida.saml.metadata.EidasMetadataConfiguration;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-import static uk.gov.ida.matchingserviceadapter.configuration.EuropeanConfigurationDefaultsHelper.getDefaultAcceptableHubConnectorEntityIds;
 
 public class EuropeanIdentityConfiguration {
-
-    @Valid
-    @JsonProperty
-    private String hubConnectorEntityId;
-
-    @Valid
-    @JsonProperty
-    private List<String> acceptableHubConnectorEntityIds;
 
     @NotNull
     @Valid
     @JsonProperty
     private boolean enabled;
-
-    @Valid
-    @JsonProperty
-    private MatchingServiceAdapterEidasMetadataConfiguration aggregatedMetadata;
-
-    public List<String> getAllAcceptableHubConnectorEntityIds(MatchingServiceAdapterEnvironment environment) {
-        Set<String> entityIds = new HashSet<>(getDefaultAcceptableHubConnectorEntityIds(environment));
-        Optional.ofNullable(hubConnectorEntityId).ifPresent(entityIds::add);
-        Optional.ofNullable(acceptableHubConnectorEntityIds).ifPresent(entityIds::addAll);
-        return new ArrayList<>(entityIds);
-    }
-
-    public EidasMetadataConfiguration getAggregatedMetadata() {
-        return aggregatedMetadata;
-    }
 
     public boolean isEnabled() {
         return enabled;
