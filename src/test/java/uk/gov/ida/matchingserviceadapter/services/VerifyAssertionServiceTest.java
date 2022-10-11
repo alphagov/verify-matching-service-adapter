95d703ec5fda064884abaff248ee333f61d077dc
james.howes <james.howes@digital.cabinet-office.gov.uk>
2022-10-11 10:37:35 +0100

    BAU - Upgrade dependencies

--- src/test/java/uk/gov/ida/matchingserviceadapter/services/VerifyAssertionServiceTest.java
+++ src/test/java/uk/gov/ida/matchingserviceadapter/services/VerifyAssertionServiceTest.java
@@ -33,7 +33,7 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
